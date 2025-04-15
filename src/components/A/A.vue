335d203e70d51ac329b8aff1adf95bba91095958
CaoKai <2655257336@qq.com>
2025-04-15 12:23:29 +0800

    重构项目结构，删除未使用的组件和样式，添加新的 A 组件及其样式，更新 ESLint 配置以禁用多词组件名称规则

---
+++ src/components/A/A.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { AProps } from './types'
+const props = withDefaults(defineProps<AProps>(), {
+  target: '_self',
+  type: 'link',
+  href: '#',
+})
+const handleClick = (e: MouseEvent) => {
+  if (props.disabled) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+}
+</script>
+
+<template>
+  <a
+    :href="props.href"
+    :target="props.target"
+    class="html-a"
+    :class="{ 'html-a--disabled': props.disabled }"
+    @click="handleClick"
+  >
+    <slot />
+  </a>
+</template>
+
+<style scoped>
+@import url('./A.css');
+</style>
---
+++ src/components/A/types.ts
@@ -0,0 +1,6 @@
+export type AProps = {
+  type?: 'link' | 'email' | 'phone'
+  href?: string
+  target?: '_blank' | '_self'
+  disabled?: boolean
+}
